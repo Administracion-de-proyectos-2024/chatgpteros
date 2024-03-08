54aba0d9ded820944731379d223662beb0996150
J Luis Vargas <vargasibarra01@gmail.com>
2024-03-08 14:27:39 -0600

    Cambio del tipo de campo para texto

--- chatgpteros/presentacion/forms.py
+++ chatgpteros/presentacion/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 # Formulario para la cajar de texto para nueva presentacion
 class TextForm(forms.Form):
-    texto_pts = forms.CharField(label='Texto')
+    texto_pts = forms.CharField(label='Texto presentacion', widget=forms.Textarea())
