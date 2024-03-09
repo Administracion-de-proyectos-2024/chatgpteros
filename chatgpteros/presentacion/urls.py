d649db7f0b08d7939bb6b008e57b95869e744f40
J Luis Vargas <vargasibarra01@gmail.com>
2024-03-08 20:01:43 -0600

    Nueva URL para mostrar una presentacion

--- chatgpteros/presentacion/urls.py
+++ chatgpteros/presentacion/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import nueva, listaPresentaciones
+from .views import nueva, listaPresentaciones, presentar
 
 urlpatterns = [
     path('', listaPresentaciones, name='lista_presentaciones'),
     path('nueva/', nueva, name='nueva'),
+    path('<int:id>',presentar, name='presentar')
 ]
