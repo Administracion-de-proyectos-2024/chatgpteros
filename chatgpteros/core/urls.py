dee314f5974d1e4012f5d667a413120975efa3df
J Luis Vargas <vargasibarra01@gmail.com>
2024-03-08 19:08:01 -0600

    Se quito url inecesaria

--- chatgpteros/core/urls.py
+++ chatgpteros/core/urls.py
@@ -19,8 +19,6 @@ from django.urls import path
 from .views import home, presentaciones,exit,register
 urlpatterns = [
     path('', home, name='home'),
-    path('presentaciones/', presentaciones, name='presentaciones'),
     path('logout/', exit, name='exit'),
     path('register/', register, name='register'),
-
 ]
