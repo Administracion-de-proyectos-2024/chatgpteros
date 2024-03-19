6797c5eef57c88365d4ca87b535304cd9c1233b8
Lesly-Yosselin-Cabral <cablesly12@gmail.com>
2024-03-19 16:19:42 -0600

    se agregan las rutas para crear y listar diapositivas en urls.py

--- chatgpteros/core/urls.py
+++ chatgpteros/core/urls.py
@@ -1,14 +1,24 @@
 
 from django.urls import path
 from .views import home, presentaciones,exit,register
-from .views import borrar_presentacion
+from .views import (
+    home,
+    presentaciones,
+    crear_diapositiva,
+    borrar_diapositiva,
+    actualizar_diapositiva,
+    lista_diapositivas,
+)
 
 urlpatterns = [
     path('', home, name='home'),
     path('presentaciones/', presentaciones, name='presentaciones'),
     path('logout/', exit, name='exit'),
     path('register/', register, name='register'),
-    path('borrar_presentacion/<int:presentacion_id>/', borrar_presentacion, name='borrar_presentacion'),
+    path('crear_diapositiva/', crear_diapositiva, name='crear_diapositiva'),
+    path('lista_diapositivas/', lista_diapositivas, name='lista_diapositivas'),
+    path('borrar_presentacion/<int:presentacion_id>/', borrar_diapositiva, name='borrar_presentacion'),
+    path('actualizar_diapositiva/<int:diapositiva_id>/', actualizar_diapositiva, name='actualizar_diapositiva'),
    
 
 ]
