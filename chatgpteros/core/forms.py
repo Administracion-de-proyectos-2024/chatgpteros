2c62961266fb62e2ce01c618d910bdebfe2cf7d6
Lesly-Yosselin-Cabral <cablesly12@gmail.com>
2024-03-19 14:14:05 -0600

    Agrega la clase DiapositivaForm para manejar el formulario de diapositivas

--- chatgpteros/core/forms.py
+++ chatgpteros/core/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Diapositiva  # Asegúrate de importar el modelo Presentacion desde tu aplicación
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+
+
+class DiapositivaForm(forms.ModelForm):
+    class Meta:
+        model = Diapositiva
+        fields = ['titulo', 'contenido']
+
