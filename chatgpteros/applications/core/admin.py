e0e542b192b4a41f69e5e8d86ee1242ac89e8be4
ErickOrtiz4756 <erickortiz4756@gmail.com>
2024-04-26 14:29:47 -0600

    Se borró la carpeta de applications

--- chatgpteros/applications/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from applications.core.models import Diapositiva, Presentacion
-
-# Register your models here.
-admin.site.register(Presentacion)
-admin.site.register(Diapositiva)
